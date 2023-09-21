b5bdb82927ca56342e2e571bb8793eb93f48d56b
cuervolu <cuervolu@protonmail.com>
2023-09-21 17:03:43 -0300

    chore: remove unused files

--- app/src/main/java/com/cuervolu/witcherscodex/ui/bestiary/BestiaryViewState.kt
+++
@@ -1,9 +0,0 @@
-package com.cuervolu.witcherscodex.ui.bestiary
-
-import com.cuervolu.witcherscodex.domain.models.Bestiary
-
-sealed class BestiaryViewState {
-    data object Loading : BestiaryViewState()
-    data class Success(val data: List<Bestiary>) : BestiaryViewState()
-    data class Error(val errorMessage: String) : BestiaryViewState()
-}
